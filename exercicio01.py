5a5f9db2eb840614b51506c927df857dd6db83de
gilbertosouto <gilbertosouto.ti@gmail.com>
2022-10-25 21:31:47 -0300

    exercicio01

---
+++ exercicio01.py
@@ -0,0 +1,14 @@
+#Crie um programa que contenha as variáveis a, b, c, d.
+
+a = 5
+b = 7.5
+c = "Olá Mundo"
+d = "true"
+
+print("0 valor da variavel A é: " + str(a) + " e o tipo dela é ")
+print(type(a))
+print("0 valor da variavel B é: " + str(b) + " e o tipo dela é ")
+print(type(b))
+print("0 valor da variavel C é: " + str(c) + " e o tipo dela é ")
+print(type(c))
+print("0 valor da variavel D é: " + str(d) + " e o tipo dela é ")
