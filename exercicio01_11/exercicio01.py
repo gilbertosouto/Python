a21eaefeac2f3dec5f26913f8410458f7e62d687
gilbertosouto <gilbertosouto.ti@gmail.com>
2022-11-01 21:10:25 -0300

    exercicio02

---
+++ exercicio01_11/exercicio01.py
@@ -0,0 +1,14 @@
+# Faça um Programa que pergunte quanto você ganha por hora
+# Número de horas trabalhadas no mês.
+# Calcule e mostre o total do seu salário no referido mês,
+# sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato
+# mostre todos os descontos, mostre o salário bruto e o líquido
+
+# Quanto pagou ao INSS.
+# Quanto pagou ao sindicato.
+# O salário líquido.
+# Calcule os descontos e o salário líquido, conforme a tabela abaixo:
+# Salário Bruto : R$ - IR (11%) : R$ - INSS (8%) : R$ - Sindicato ( 5%)
+# Salário Liquido : R$
+# Obs.: Salário Bruto - Descontos = Salário Líquido.
+
---
+++ exercicio01_11/exercicio02.py
@@ -0,0 +1,25 @@
+# • Faça um programa para imprimir igual abaixo:
+# 1
+# 2 2
+# 3 3 3 ...
+# n n n n ...
+
+# Programa impressão números e Letras!
+
+def enes(n):
+    for c in range(1, n + 1):
+        cont = 1
+        while True:
+            print(c, end=' ')
+            if cont == c:
+                break
+            cont += 1
+        print()
+
+n = int(input('Digite um número: '))
+
+for i in range(n):
+        i += 1
+        print(str(i) * i)
+n = int(input('Digite um número: '))
+
